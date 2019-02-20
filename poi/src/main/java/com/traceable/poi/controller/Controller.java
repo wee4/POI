66cee0adc2ec66118b7d695c1394b18e2a234271
Wesley Fermino da Cruz <wesleycz@live.com>
2019-02-20 01:51:47 -0300

    + relationships modifications and expose end point of meetings

--- poi/src/main/java/com/traceable/poi/controller/Controller.java
+++ poi/src/main/java/com/traceable/poi/controller/Controller.java
@@ -13,6 +13,7 @@ import com.traceable.poi.services.PoiService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,8 @@ public class Controller {
 
     @Autowired
     private MeetingRepository meetingRepository;
-
+    
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/vehicles")
     public List<Vehicle> getAllVehicles() {
         return vehicleRepository.findAll();
